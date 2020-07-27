cf851cb3e174aa28913e1a8641b383ffc40595de
Lukhanyo Vakele <47126706+LukhanyoV@users.noreply.github.com>
2020-07-27 08:54:15 +0200

    Save current state

---
+++ precache-manifest.895235018c46378c57141aacb3fcbdb7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ad1f87a78988512110fd0a459369106",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a15cc4d5f18c8cf5e4a",
+    "url": "/static/css/main.b173fc6f.chunk.css"
+  },
+  {
+    "revision": "adc19fd702b90ae91480",
+    "url": "/static/js/2.fdbdbfa8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fdbdbfa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a15cc4d5f18c8cf5e4a",
+    "url": "/static/js/main.00af6867.chunk.js"
+  },
+  {
+    "revision": "0e1bcb3d283b9a894eda",
+    "url": "/static/js/runtime-main.a014107c.js"
+  },
+  {
+    "revision": "a3d1813f47d9fae198907483ed00098b",
+    "url": "/static/media/avatar.a3d1813f.png"
+  },
+  {
+    "revision": "48b9b06a772fba4935047f932208b031",
+    "url": "/static/media/avatars.48b9b06a.svg"
+  },
+  {
+    "revision": "3e8bb13aa70353ca17e29a285225c9ca",
+    "url": "/static/media/finder.3e8bb13a.png"
+  },
+  {
+    "revision": "d6f27f3e0c246e90fd789015c6e88206",
+    "url": "/static/media/keeper.d6f27f3e.png"
+  },
+  {
+    "revision": "d3fce76ffdf21a049a02a269dcd91906",
+    "url": "/static/media/password.d3fce76f.jpg"
+  }
+]);
